ca87816e3b5fb6785d1951c9c2bd1a5e4ff57432
samspei0l <omkaryash123@gmail.com>
2025-07-12 20:53:57 +0530

    Adding Vercel Speed Insights and Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
 import '../styles/globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Component {...pageProps} />
+    <SpeedInsights />
+    <Analytics />
+
+  </>
+  );
 }
+export default App;
+
+
